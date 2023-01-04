132ebc8af8ca0496d959a2eb9717a4672cc4b723
abhishek <abhishekshrivastav1920@gmail.com>
2023-01-04 12:35:28 +0530

    login page done UI

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./login.scss"
+function Login() {
+    return (
+        <div className='login'>
+            <div className='card'>
+
+                <div className='left'>
+                    <h1> Hello World</h1>
+                    <p> Looks like there is some padding/margin space somewhere creating that. Can't really say where because all you've given us to work off of are image</p>
+                    <span> Don't you have a account?</span>
+
+                    <button>Register</button>
+                </div>
+                <div className='right'>
+                    <h1> Login</h1>
+                    <form>
+                        <input type='text' placeholder='Username'></input>
+                        <input type='password' placeholder='Password'></input>
+                        <button> Login </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
